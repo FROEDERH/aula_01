6263e61a0f5f6c731ef45acabddf9176782b2585
Henrique Froeder <henriquefroeder52@gmail.com>
2021-09-04 09:24:04 -0300

    first commit

---
+++ imc/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace imc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=============");
+            Console.WriteLine("Calcular IMC");
+            Console.WriteLine("=============\n");
+
+            Console.WriteLine("Informe seu peso: ");
+            int peso = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe sua altura: ");
+            Double altura = Convert.ToDouble(Console.ReadLine());
+
+            calcularImc(peso, altura);
+
+
+        }
+        static void calcularImc(int peso, Double altura)
+        {
+            int imc =Convert.ToInt32 ( peso / (altura * altura)) ;
+
+            Console.WriteLine(@$"Seu IMC é = {imc} ");
+
+            if (imc < 20)
+                Console.WriteLine(" (abaixo do peso).");
+            else if ((imc >= 20) && (imc < 25))
+                Console.WriteLine(" (peso normal).");
+            else if ((imc >= 25) && (imc < 30))
+                Console.WriteLine(" (acima do peso).");
+            else if ((imc >= 30) && (imc < 34))
+                Console.WriteLine(" (obeso).");
+            else
+                Console.WriteLine(" (muito obeso).");
+        }
+
+
+
+
+
+
+
+
+    }
+    }
+
